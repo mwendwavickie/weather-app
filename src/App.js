a981b8509ef25c9fbf60925f4cef06c4aea9057b
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-10-24 23:06:00 +0300

    Style the form input and the button.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { WiThermometer, WiDaySunny, WiRaindrop, WiStrongWind } from 'react-icons/wi';
-//import './App.css';
+import './App.css';
 
 const App = () => {
 
